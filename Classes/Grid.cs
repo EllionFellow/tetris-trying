b7d31245373956e788067562b9fc2735087b268c
Алексей Сороцкий <peterbishop13@gmail.com>
2020-05-18 17:35:04 +0300

    The first github synchro

---
+++ Classes/Grid.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTry.Classes
+{
+    class Grid
+    {
+        #region //container
+        private int widthGrid;
+        private int heightGrid;
+        private bool[,] table;
+        #endregion
+
+        public int Width
+        {
+            get { return widthGrid; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    widthGrid = value;
+                }
+            }
+        }
+        public int Height
+        {
+            get { return heightGrid; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    heightGrid = value;
+                }
+            }
+        }
+        public int Space { get; }
+        public bool[,] Table { get { return table; }}
+
+        #region //ctor
+        public Grid(int width, int height)
+        {
+            widthGrid = width;
+            heightGrid = height;
+            Space = widthGrid * heightGrid;
+            table = new bool[width, height];
+        }
+        #endregion
+
+        public void DrawGrid(PaintEventArgs e, Grid grid, float reW,float reH)
+        {
+            for (int x = 0; x < grid.Width + 1; x++)
+            {
+                e.Graphics.DrawLine(Pens.Black, x * reW + 1, 0.0f + 1+23, x * reW + 1, grid.Height * reH + 1+23);
+            }
+            for (int y = 0; y < grid.Height + 1; y++)
+            {
+                e.Graphics.DrawLine(Pens.Black, 0.0f + 1, y * reH + 1+23, grid.Width * reW + 1, y * reH + 1+23);
+            }
+            for (int x = 0; x < grid.Width; x++)
+            {
+                for (int y = 0; y < grid.Height; y++)
+                {
+                    if (grid.table[x, y] == true)
+                        e.Graphics.DrawRectangle(Pens.Red, x * reW + 3, y * reH + 26, reW-4, reH -4);
+                }
+            }
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using MyTry.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTry
+{
+    public partial class Form1 : Form
+    {
+        float resizeMultiplierH, resizeMultiplierW;
+
+        Grid setka;
+        public Form1()
+        {
+            InitializeComponent();
+            setka = CreateGrid(20, 10);
+            resizeMultiplierH = ((Height - 43)/ setka.Height);
+            resizeMultiplierW = ((Width -17 - 100) / setka.Width);
+        }
+
+        private Grid CreateGrid(int h, int w)
+        {
+            Grid grid = new Grid(w, h);
+            return grid;
+        }
+
+        
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            resizeMultiplierH = ((Height - 43) / setka.Height);
+            resizeMultiplierW = ((Width - 17 -100) / setka.Width);
+            this.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s,x) =>
+                {
+                    RandomSquare();
+
+                };
+        }
+
+        private void RandomSquare()
+        {
+            Random random = new Random();
+            setka.Table[random.Next(setka.Width), random.Next(setka.Height)] = true;
+            this.Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            setka.DrawGrid(e, setka, resizeMultiplierW, resizeMultiplierH);
+        }
+    }
+}
